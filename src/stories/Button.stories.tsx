7e45c64179306d9f2ace2fcf1949465263e0cf11
Wei Su <cs.akasuv@gmail.com>
2022-05-18 20:54:42 +0800

    feat: init

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -8,7 +8,7 @@ export default {
 
 const Template = (args: any) => <Button {...args} />;
 
-export const Default = Template.bind({
+export const Default: any = Template.bind({
 
 
 });
--- src/stories/NFTCard.stories.tsx
+++ src/stories/NFTCard.stories.tsx
@@ -1,14 +1,14 @@
-import { ComponentStory } from "@storybook/react";
+import { ComponentStory, Meta } from "@storybook/react";
 import NFTCard from "../components/NFTCard";
 
 export default {
   title: "Features/NFTCard",
   component: NFTCard,
-};
+} as Meta;
 
-const Template = (args) => <NFTCard {...args} />;
+const Template = (args: any) => <NFTCard {...args} />;
 
-export const Default = Template.bind({});
+export const Default: any = Template.bind({});
 Default.args = {
   name: "Bored Ape Yacht Club",
   imageURL: "https://ik.imagekit.io/bayc/assets/bayc-footer.png",
