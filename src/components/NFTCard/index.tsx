ddd168b858c1db081b53002bdaf4105cb3c4ac4e
Wei Su <cs.akasuv@gmail.com>
2022-05-21 23:30:45 +0800

    feat: create nft card component

--- src/components/NFTCard/index.tsx
+++ src/components/NFTCard/index.tsx
@@ -5,29 +5,99 @@ import Typography from "@mui/material/Typography";
 export type NFTCardProps = {
   name: string;
   imageURL: string;
+  contractAddress: string;
+  backgroundImage: string;
 };
 
-const NFTCard = ({ name, imageURL }: NFTCardProps) => {
+const NFTCard = ({
+  name,
+  imageURL,
+  contractAddress,
+  backgroundImage,
+}: NFTCardProps) => {
   return (
     <Box
       sx={{
         boxSizing: "border-box",
-        height: 339,
-        width: 226,
-        border: "3px solid #fff",
-        backgroundColor: "#000",
+        height: 180,
+        width: 300,
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "relative",
         color: "#fff",
-        px: 1.6,
-        pt: 2.4,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        borderRadius: "8px",
+        overflow: "hidden",
+        fontFamily: "IBM Plex Sans",
       }}
     >
-      <Box sx={{ textAlign: "center" }}>
-        <Typography fontSize={20}>{name}</Typography>
+      <Box
+        sx={{
+          zIndex: 2,
+          position: "relative",
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "spaceBetween",
+          height: "100%",
+          boxSizing: "border-box",
+        }}
+      >
+        <Box sx={{ height: 20, mb: 1 }}>
+          <img src="/logo.svg" alt="nft100 logo" width={76} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            lineHeight={1.3}
+            fontSize={30}
+            fontWeight={700}
+            textAlign="center"
+          >
+            {name}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            height: 36,
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box>
+            <Typography fontSize={10} color="rgba(255, 255, 255, 0.7)">
+              Contract
+            </Typography>
+            <Typography fontSize={10} fontWeight="medium">
+              {contractAddress}
+            </Typography>
+          </Box>
+          <Box sx={{ width: 36, height: 36 }}>
+            <img src={imageURL} width="100%" height="100%" alt={name} />
+          </Box>
+        </Box>
       </Box>
-      <img alt={name} src={imageURL} width={200} height={200} />
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.42)",
+          backdropFilter: "blur(8px)",
+          zIndex: 1,
+          borderRadius: "8px",
+          overflow: "hidden",
+        }}
+      />
     </Box>
   );
 };
--- src/stories/NFTCard.stories.tsx
+++ src/stories/NFTCard.stories.tsx
@@ -12,4 +12,7 @@ export const Default: any = Template.bind({});
 Default.args = {
   name: "Bored Ape Yacht Club",
   imageURL: "https://ik.imagekit.io/bayc/assets/bayc-footer.png",
+  contractAddress: "0x49cf6f5d44e7...053f30ada28b",
+  backgroundImage:
+    "https://img.seadn.io/files/00e5b31155563c68db56ef79eacbb364.png?auto=format&h=720&w=720",
 };
